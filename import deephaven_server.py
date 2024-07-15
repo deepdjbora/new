fafe558babc350278ec3107af78c0956492fa6fd
deepdjbora <deepdjbora@rediff.com>
2024-07-15 16:21:47 +0530

    new

--- import deephaven_server.py
+++
@@ -1,7 +0,0 @@
-import deephaven_server
-
-server = deephaven_server.Server(port=10000, jvm_args=["-Xmx4g"])  # Adjust memory as needed
-server.start()
-
-import deephaven as dh  # Import Deephaven modules after starting the server
-
